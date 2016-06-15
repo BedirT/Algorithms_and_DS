7506fd13d77687bc4381babeae663372d0460667
BedirT <bedir.tapkan.53@gmail.com>
2016-06-15 15:45:51 +0700

    Geometry added

---
+++ Algorithms/Geometry/geo_lineIntersection.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+struct point {
+    int x, y;
+};
+
+int direction (point p1, point p2, point p3) {
+    return (p2.y - p1.y) * (p3.x - p2.x) - (p3.y - p2.y) * (p2.x - p1.x);
+}
+
+bool onSeg (point P1, point P2, point P3) {
+    if ( min(P1.x, P2.x) <= P3.x && P3.x <= max(P1.x, P2.x) )
+        if ( min(P1.y, P2.y) <= P3.y && P3.y <= max(P1.y, P2.y) )
+            return true;
+    return false;
+}
+
+bool segIntersect (point p1, point p2, point p3, point p4) {
+    
+    int d1 = direction(p3,p4,p1); int d2 = direction(p3,p4,p2);
+    int d3 = direction(p1,p2,p3); int d4 = direction(p1,p2,p4);
+    
+    if ( ((d1>0 && d2<0) || (d1<0 && d2>0)) && ((d3>0 && d4<0) || (d3<0 && d4>0)) )
+        return true;
+    else if ( d1 == 0 && onSeg(p3,p4,p1) ) return true;
+    else if ( d2 == 0 && onSeg(p3,p4,p2) ) return true;
+    else if ( d3 == 0 && onSeg(p1,p2,p3) ) return true;
+    else if ( d4 == 0 && onSeg(p1,p2,p4) ) return true;
+    else return false;
+    
+}
+
+int main () {
+
+    // We will have four point to check the lines
+    // 2 for one lines edges, 2 for the other one
+    point line1p1;
+    point line1p2;
+    point line2p1;
+    point line2p2;
+    
+    // Defining Line 1
+    line1p1.x = 2, line1p1.y = 8; // [2,8]
+    line1p2.x = 2, line1p1.y = 2; // [2,2]
+    
+    // Defining Line 2
+    line2p1.x = 0, line2p1.y = 4; // [0,4]
+    line2p2.x = 6, line2p2.y = 4; // [6,4]
+    
+    if ( segIntersect( line1p1, line1p2, line2p1, line2p2 ) )
+        cout << "YES" ;
+    else
+        cout << "NO" ;
+    
+}
