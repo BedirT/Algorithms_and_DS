2a82533ad3dc34a6950860c5457effa7a86bd12a
BedirT <bedir.tapkan.53@gmail.com>
2016-06-22 19:59:30 +0700

    counting sorts

--- Problems/HackerRank/Sorting/Counting Sort 3.cpp
+++ Problems/HackerRank/Sorting/Counting Sort 3.cpp
@@ -4,23 +4,35 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int i,n;
-
-int main() {
+int i;
+void count_sort(int* arr, int n){
     
-    cin >> n;
-    
-    int arr[n];
     int counter[100] = {0};
     
-    for (i=0 ; i<n ; i++) {
-        cin >> arr[i];
+    for (i=0; i<n; i++)
         counter[arr[i]]++;
-    }
     
-    for (i=0 ; i<100 ; i++) {
+    int output = 0;
+    
+    for (i=1; i< 100; i++)
+        counter[i] += counter[i-1];
+    
+    for(i = 0 ; i < 100 ; i++)
         cout << counter[i] << " ";
-    }
+    cout << endl;
+    
+}
+
+int main() {
+    int n;
+    cin >> n; // Array's size
+    int arr[n];
+    string s;
+    
+    for(i = 0 ; i < n ; i++)
+        cin >> arr[i] >> s;
+    
+    count_sort(arr, n);
     
     return 0;
 }
