a152a9435089e65d366ff170889372ea32548d85
BedirT <bedir.tapkan.53@gmail.com>
2016-07-13 17:17:54 -0500

    Extra Long Substruction Algo Added

--- Algorithms/Number Theory/extra_long_sub.cpp
+++ Algorithms/Number Theory/extra_long_sub.cpp
@@ -1,4 +1,4 @@
-// Summation 
+// Subtruction
 
 #include<iostream>
 #include<string>
@@ -9,25 +9,25 @@ void extraLong (string str1, string str2) {
     
     int i;
     int maxLen = (str1.size() > str2.size()) ? str1.size() : str2.size();
-    int num1[str1.size()], num2[str2.size()], sum[maxLen+1];
+    int num1[str1.size()], num2[str2.size()], sub[maxLen];
+    
+    memset(num1, 0, sizeof(num1));
+    memset(num2, 0, sizeof(num2));
+    memset(sub, 0, sizeof(sub));
     
     for (i = 0; i < str1.size(); ++i) num1[i] = str1[str1.size()-i-1] - '0';
     for (i = 0; i < str2.size(); ++i) num2[i] = str2[str2.size()-i-1] - '0';
     
-    for (i = 0; i < maxLen+1 ; ++i) sum[i] = num1[i] + num2[i] ;
-    
-    for (i = 0; i < maxLen+1 ; ++i){
-        sum[i+1] += sum[i]/10;
-        sum[i] %= 10;
+    for (i = 0; i < maxLen ; ++i) {
+        if (num1[i] < num2[i]) {
+            num1[i+1]--;
+            num1[i] += 10;
+        }
+        sub[i] = num1[i] - num2[i];
     }
     
-    if (sum[maxLen]/10 == 1){
-        sum[maxLen+1] = sum[maxLen]/10;
-        sum[maxLen] %= 10;
-    }
-    
-    for (int i = 0; i < maxLen ; ++i)
-        cout << sum[maxLen-i-1];
+    for (int i = 0; i < str1.size() ; ++i)
+        cout << sub[str1.size()-i-1];
     cout << endl;
 }
 
@@ -36,6 +36,7 @@ int main () {
     string a, b; // Both Long integers
     
     cin >> a >> b; // Both Long integers << inputted
+    // First one should be longer ...
     
     extraLong(a, b);
     
