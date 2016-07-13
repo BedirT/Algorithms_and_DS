4cfb7aba5b3b317e3832615ab11c5dab2745b75a
BedirT <bedir.tapkan.53@gmail.com>
2016-07-13 16:34:53 -0500

    Extra Long Sum Algo Added

---
+++ Algorithms/Number Theory/extra_long_sum.cpp
@@ -0,0 +1,42 @@
+// 
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void extraLong (string str1, string str2) {
+    
+    int i;
+    int maxLen = (str1.size() > str2.size()) ? str1.size() : str2.size();
+    int num1[str1.size()], num2[str2.size()], sum[maxLen+1];
+    
+    for (i = 0; i < str1.size(); ++i) num1[i] = str1[str1.size()-i-1] - '0';
+    for (i = 0; i < str2.size(); ++i) num2[i] = str2[str2.size()-i-1] - '0';
+    
+    for (i = 0; i < maxLen+1 ; ++i) sum[i] = num1[i] + num2[i] ;
+    
+    for (i = 0; i < maxLen+1 ; ++i){
+        sum[i+1] += sum[i]/10;
+        sum[i] %= 10;
+    }
+    
+    if (sum[maxLen]/10 == 1){
+        sum[maxLen+1] = sum[maxLen]/10;
+        sum[maxLen] %= 10;
+    }
+    
+    for (int i = 0; i < maxLen ; ++i)
+        cout << sum[maxLen-i-1];
+    cout << endl;
+}
+
+int main () {
+
+    string a, b; // Both Long integers
+    
+    cin >> a >> b; // Both Long integers << inputted
+    
+    extraLong(a, b);
+    
+}
