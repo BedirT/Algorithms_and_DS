ffe55d0137eeb56121c0cae62403c3a3dcd90e1c
BedirT <bedir.tapkan.53@gmail.com>
2016-07-13 15:49:03 -0500

    Curriculums 4th Week Solution _ Sieve Of Era... Solution added

---
+++ Problems/Curriculum Q's/Week 4/sieve question.cpp
@@ -0,0 +1,37 @@
+// https://wiki.haskell.org/99_questions/Solutions/39
+// Application Of Sieve of Eratosthenes
+
+#include<iostream>
+using namespace std;
+
+void sieve (int l, int h){
+    
+    int j,i;
+    int array[h];
+    bool was[h];
+    memset(was, true , sizeof(was));
+    
+    for (i = 2 ; i <= h ; ++i) {
+        if (was[i]){
+            for (j = i*i; j <= h; j += i) {
+                was[j] = false;
+            }
+        }
+    }
+    
+    for (i = 2; i <= h ; ++i ) {
+        if (was[i]) cout << i << endl ;
+    }
+    
+}
+
+int main () {
+
+    int l, h; // Upper and Lower Limits
+    
+    // Enter Lower limit and then Upper limit
+    cin >> l >> h;
+    
+    sieve(l, h);
+    
+}
