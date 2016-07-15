9905eadea4c4a77bb6a34cc9e67065b46ffc4417
BedirT <bedir.tapkan.53@gmail.com>
2016-07-14 23:10:42 -0500

    BFS added
    Breadth First Search added to Graph file

---
+++ Algorithms/Graph/bfs.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#define maxNum 101
+using namespace std;
+
+vector<int> ways[maxNum];
+
+int line[maxNum];
+int n, m, a, b, i, v, u, strt;
+stack<int> holder;
+
+void BFS(){
+
+    line[strt] = 0;
+    holder.push(strt);
+    
+    while (!holder.empty()) {
+        
+        u = holder.top();
+        holder.pop();
+        
+        for (i = 0; i < ways[u].size() ; ++i) {
+            v = ways[u][i];
+            if (line[v] == -1) {
+                line[v] = line[u] + 1;
+                holder.push(v);
+            }
+        }
+    }
+}
+
+int main(){
+
+    cin >> n >> m;
+    
+    for (i = 0 ; i < m ; ++i) {
+        cin >> a >> b;
+        ways[a].push_back(b);
+        ways[b].push_back(a);
+    }
+    
+    cin >> strt;
+    
+    memset(line, -1, sizeof(line));
+    
+    BFS();
+    
+    // We will print how far is the nodes to the start point
+    for (i = 0; i < n ; ++i)
+        cout << line[i] << " ";
+    cout << endl;
+
+}
