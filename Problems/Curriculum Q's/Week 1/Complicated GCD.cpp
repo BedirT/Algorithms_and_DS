a89afee69e8538c215f909206a68623d515d27a9
BedirT <bedir.tapkan.53@gmail.com>
2016-08-10 22:03:13 -0500

    Complicated GCD Added

---
+++ Problems/Curriculum Q's/Week 1/Complicated GCD.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    
+    string a, b;
+    cin >> a >> b;
+    
+    if (a == b) cout <<  b  << endl;
+    else        cout << "1" << endl;
+    
+}
