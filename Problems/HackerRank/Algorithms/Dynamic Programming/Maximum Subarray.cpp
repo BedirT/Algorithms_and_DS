5d09820ce3d996cfc8818a3eca33880c59560d18
BedirT <bedir.tapkan.53@gmail.com>
2016-09-18 22:45:31 -0500

    Maximum Subarray added

--- Problems/HackerRank/Algorithms/Dynamic Programming/Maximum Subarray.cpp
+++ Problems/HackerRank/Algorithms/Dynamic Programming/Maximum Subarray.cpp
@@ -4,34 +4,34 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+int t, n;
+int kadane(int * arr, int n){
+    int cmx = arr[0], gmx = arr[0];
+    for(int i = 1 ; i < n ; ++i){
+        cmx = max(arr[i], arr[i]+cmx);
+        if(cmx >= gmx) gmx = cmx;
+    }
+    return gmx; 
+}
 
 int main() {
-    int t, n;
+    
     cin >> t;
     while(t--){
-        
         cin >> n;
-        int arr[n+2];
-        int d[n];
-        d[0] = 0;
-        
-        int allMax = 0;
-        int contMax;
-        int maxMin = -999999;
-        for(int i = 1; i <= n; ++i){
+        int arr[n+1];
+        int maxx = 0;
+        int topmin = -99999;
+        for(int i = 0 ; i < n ; ++i){
             cin >> arr[i];
-            if(i == 1)contMax = arr[1];
-            d[i] = arr[i] + d[i-1];
-            if(contMax < d[i]) contMax = d[i];
-            if(arr[i] <= 0 && maxMin < arr[i]) maxMin = arr[i];
-            if(arr[i] > 0) allMax += arr[i];
-            if(contMax < arr[i]) contMax = arr[i];
+            if(arr[i] < 0 && topmin < arr[i]) topmin = arr[i];
+            if(arr[i] >= 0) maxx += arr[i];
         }
-        
-        if(allMax == 0) allMax = maxMin;
-        cout << contMax << " " << allMax << endl;
+        if(maxx == 0 && topmin != -99999) maxx = topmin;
+        cout << kadane(arr, n) << " " << maxx << endl;
         
     }
+    
     
     return 0;
 }
