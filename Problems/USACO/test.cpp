9743f87234b89e3a398cdd3bc46944bf4a130a2e
BedirT <bedir.tapkan.53@gmail.com>
2016-11-08 14:15:15 -0600

    USACO training started

---
+++ Problems/USACO/test.cpp
@@ -0,0 +1,19 @@
+/*
+ID: bedir.t1
+PROG: test
+LANG: C++11
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+    int a, b;
+    fin >> a >> b;
+    fout << a+b << endl;
+    return 0;
+}
