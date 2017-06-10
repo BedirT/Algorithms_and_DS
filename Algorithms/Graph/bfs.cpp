536ab797e481bd500129de4b97af67f0078d4269
BedirT <mtapkan@na.edu>
2017-06-10 18:42:34 -0500

    bfs updated

--- Algorithms/Graph/bfs.cpp
+++ Algorithms/Graph/bfs.cpp
@@ -1,27 +1,26 @@
-#include<iostream>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
 #define maxNum 101
 using namespace std;
 
-vector<int> ways[maxNum];
+void BFS(vector<vector<int> > ways, int strt){
 
-int line[maxNum];
-int n, m, a, b, i, v, u, strt;
-queue<int> holder;
-
-void BFS(){
+    queue<int> holder;
+    int line[maxNum];
+    memset(line, -1, sizeof line);
 
     line[strt] = 0;
     holder.push(strt);
-    
+
     while (!holder.empty()) {
         
-        u = holder.front();
+        int u = holder.front();
         holder.pop();
         
-        for (i = 0; i < ways[u].size() ; ++i) {
-            v = ways[u][i];
+        for (int i = 0; i < ways[u].size() ; ++i) {
+            int v = ways[u][i];
             if (line[v] == -1) {
                 cout << v << " ";
                 line[v] = line[u] + 1;
@@ -33,9 +32,16 @@ void BFS(){
 
 int main(){
 
+    vector<vector<int> > ways;
+    int line[maxNum];
+    int n, m, a, b, strt;
+    queue<int> holder;
+
     cin >> n >> m;
     
-    for (i = 0 ; i < m ; ++i) {
+    ways.resize(n);
+
+    for (int i = 0 ; i < m ; ++i) {
         cin >> a >> b;
         ways[a].push_back(b);
         ways[b].push_back(a);
@@ -45,7 +51,7 @@ int main(){
     
     memset(line, -1, sizeof(line));
     
-    BFS();
+    BFS(line, holder, ways, strt);
     
     // We will print how far is the nodes to the start point
 //    for (i = 0; i < n ; ++i)
