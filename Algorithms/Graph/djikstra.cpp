61b8b058d11e8835a6e5672a89432336ad53ad53
BedirT <mtapkan@na.edu>
2017-06-11 18:21:16 -0500

    Djikstra updated

--- Algorithms/Graph/djikstra.cpp
+++ Algorithms/Graph/djikstra.cpp
@@ -1,49 +1,52 @@
-#include<iostream>
-#include<cstring>
-#define inf 0x7fffffff
+#include <iostream>
+#include <vector>
+#include <cstring>
+#define INF 0xfffffff
+#define maxN 101 // Maximum number of nodes
 using namespace std;
 
+void djikstra(int n, int s, int end, int ar[maxN][maxN], int * d){
+    // THE NODES USED ARE 1 INDEXED (NUMBERED FROM 1-N)...
+    // Initializing the variables
+    bool visited[maxN];
+    for(int i = 0; i <= n; ++i) d[i] = INF, visited[i] = false;
+    d[s] = 0; 
+
+    for(int ct = 0; ct < n ; ++ct){
+        for(int i = 1; i <= n; ++i)
+            if(ar[s][i] != -1 && d[i] > d[s] + ar[s][i])
+                d[i] = d[s] + ar[s][i];
+            
+        visited[s] = true;
+
+        int mn = INF;
+        for(int i = 1; i <= n; ++i)
+            if(mn > d[i] && !visited[i])
+                mn = d[i], s = i;   // continue from minimum that
+    }                               // isn't visited
+
+    for(int i = 1; i <= n; ++i){
+        printf("%d ", d[i]);
+    }printf("\n");
+}
+
 int main () {
-    
-    int n, m, s, a, b, k;
-    cin >> n >> m;
-    int arr[n+1][n+1];
-    bool was[n+1];
-    int d[n+1];
-    
-    for (int i = 0; i <= n; ++i) {
-        d[i] = inf;
-        was[i] = false;
+    int t, n, e, m;
+    int ar[maxN][maxN], d[maxN];
+
+    scanf("%d", &t); // Testcases
+    while(t--){
+        scanf("%d %d %d", &n, &e, &m);
+
+        memset(ar, -1, sizeof ar);
+
+        for(int i = 0, a, b, w; i < m; ++i){
+            scanf("%d %d %d", &a, &b, &w);
+            ar[a][b] = w;
+        }
+
+        // s: starting point, e: ending point, n: number of nodes
+        djikstra(n, s, e);
+
     }
-    
-    memset(arr, -1, sizeof arr);
-    
-    for(int i = 0; i < m ; ++i){
-        cin >> a >> b >> k;
-        arr[a][b] = k;
-    }
-    cin >> s;
-    
-    d[s] = 0;
-    
-    int ct = n;
-    while(ct--){
-        
-        for(int i = 0 ; i < n ; ++i)
-            if (arr[s][i] != -1 && d[i] > d[s] + arr[s][i])
-                d[i] = d[s] + arr[s][i];
-        
-        was[s] = true;
-        int min = inf;
-        for (int i = 0; i < n ; ++i)
-            if ( min > d[i] && !was[i]){
-                min = d[i];
-                s = i;
-            }
-    }
-    
-    for (int i = 0; i < n; ++i) {
-        cout << d[i] << " " ;
-    }cout << endl;
-    
 }
