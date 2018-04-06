5ac3f8c5f12f3c2208cb5478fa6326ec3d57fd11
BedirT <mtapkan@na.edu>
2018-04-06 09:11:12 -0500

    sieve updated

--- Algorithms/Math/Sieve of Eratorthenes.cpp
+++ Algorithms/Math/Sieve of Eratorthenes.cpp
@@ -1,37 +1,44 @@
+// sieve of eratorthenes
+
+// Desc: Find the prime numbers from 1 - n
+// where n is type 'long'.
+
 #include <iostream>
-#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-void sieveOfE(bool was, long n){
-    
-    long i,j;
-    
-    for (i = 2; i*i <= n; i++)
-        if (!was[i])
-            for (j = i*i; j <= n ; j+=i )
-                was[j] = true;
-    
-    // For Printing
-    // for (i = 2; i <= n; i++)
-    //     if (was[i])
-    //         cout << i << " ";
-    
+// n -> max number that I will check
+// marked -> bool arr, that I mark composite nums
+// primes -> vector that contains primes from 2->n
+void sieve(long n, bool * marked, vector<int> primes){
+
+	// O(n*sqrt(n)) - but much more faster in real time
+	for (long i = 2; i <= n; ++i){ 	
+	// we will check 2->n if they are already marked
+		if (!marked[i]){
+			// Checking if prime
+			primes.push_back(i);
+			for (long j = i*i; j <= n; j += i){
+				marked[j] = true;
+			}
+		}
+	}
+}
+void printVector(vector<int> vc) {
+	for (int i = 0; i < vc.size(); ++i)
+		cout << vc[i] << " ";
+	cout << endl << endl;
 }
 
 int main() {
-    
-    long n;
-    cin >> n; // The limit for us to search prime numbers
-    bool was[n];
-    sieveOfE(was, n);
-    for(int i = 0; i < n; ++i){
-        if(was[i]){
-            sum += i;
-            if(sum >= 2000000000) {
-                break;
-            }
-        }
-    }
-    cout << sum << endl;
-    
+	long n;
+	cin >> n;
+
+	bool marked[n+1];
+	vector<int> primes;
+
+	sieve(n, marked, primes);
+	printVector(primes);
 }
+
