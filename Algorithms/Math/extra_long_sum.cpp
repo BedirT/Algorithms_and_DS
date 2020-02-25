d1d88261efcffa9affe23c7973900266dc83ab5a
Bedir Tapkan <tapkan@ualberta.ca>
2020-02-24 18:03:28 -0700

    fixed the allocation issue

--- Algorithms/Math/extra_long_sum.cpp
+++ Algorithms/Math/extra_long_sum.cpp
@@ -2,27 +2,37 @@
 
 #include<iostream>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
 void extraLong (string str1, string str2) {
     
     int i;
+    int * num1, *num2;
     int maxLen = (str1.size() > str2.size()) ? str1.size() : str2.size();
-    int num1[str1.size()], num2[str2.size()], sum[maxLen+1];
+    if(str1.size() > str2.size()){
+        num1 = new int[str1.size()], num2 = new int[str2.size()];
+        for (i = 0; i < str1.size(); ++i) num1[i] = str1[str1.size()-i-1] - '0';
+        for (i = 0; i < str2.size(); ++i) num2[i] = str2[str2.size()-i-1] - '0';
+    }
+    else{
+        num1 = new int[str2.size()], num2 = new int[str1.size()];
+        for (i = 0; i < str1.size(); ++i) num2[i] = str1[str1.size()-i-1] - '0';
+        for (i = 0; i < str2.size(); ++i) num1[i] = str2[str2.size()-i-1] - '0';
+    }
+        
+    int sum[maxLen+1];
     
-    memset(sum , 0 ,sizeof(sum));
+    memset(sum, 0, sizeof(sum));
     
-    for (i = 0; i < str1.size(); ++i) num1[i] = str1[str1.size()-i-1] - '0';
-    for (i = 0; i < str2.size(); ++i) num2[i] = str2[str2.size()-i-1] - '0';
+    for (i = 0; i < maxLen ; ++i) sum[i] = num1[i] + num2[i];
     
-    for (i = 0; i < maxLen ; ++i) sum[i] = num1[i] + num2[i] ;
-    
-    for (i = 0; i < maxLen ; ++i){
+    for (i = 0; i <= maxLen ; ++i){
         sum[i+1] += sum[i]/10;
         sum[i] %= 10;
     }
-    
+
     if (sum[maxLen] == 1) cout << "1";
     for (int i = 0; i < maxLen ; ++i)
         cout << sum[maxLen-i-1];
