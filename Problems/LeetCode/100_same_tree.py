65aa7d6dc8ccdf8c0dd934143d8e22056414d9d0
Bedir Tapkan <tapkan@ualberta.ca>
2022-07-05 16:21:12 -0700

    same tree added

---
+++ Problems/LeetCode/100_same_tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def traverse(self, root):
+        if root is None:
+            return "N"
+        ans = str(root.val)
+        ans += self.traverse(root.left) if root.left else 'N'
+        ans += self.traverse(root.right) if root.right else 'N'
+        return ans
+        
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        a = self.traverse(p)
+        b = self.traverse(q)
+        return a == b
+        
